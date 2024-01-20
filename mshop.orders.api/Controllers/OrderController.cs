62948fab86c2ab2f038777855a593074e062e331
Konrad Łatwiński <konrad.latwinski@gmail.com>
2024-01-20 16:31:35 +0100

    feat: add order dto

---
+++ mshop.orders.api/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mshop.orders.application.DTOs;
+
+namespace mshop.orders.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult GetOrders([FromBody] OrderDto orderDto)
+        {
+            return Ok("hey"); 
+        }
+    }
+}
---
+++ mshop.orders.application/DTOs/OrderDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mshop.orders.application.DTOs
+{
+    public class OrderDto
+    {
+        public string? Id { get; set; }
+
+        public string ClientEmail { get; set; } = null!;
+
+        public List<string> IdProducts { get; set; } = null!;
+
+        public decimal TotalPriceBeforeDiscount { get; set; }
+
+        public decimal TotalPriceAfterDiscount { get; set; }
+        public decimal DiscountPercentage { get; set; }
+    }
+}
